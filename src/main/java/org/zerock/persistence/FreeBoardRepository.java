5e2d749af4e37a7b085205238246829996c85ff1
SexyTreeTrunks <apenr1234@gmail.com>
2018-03-02 11:33:05 +0900

    delete unused method in FreeBoardRepository

--- src/main/java/org/zerock/persistence/FreeBoardRepository.java
+++ src/main/java/org/zerock/persistence/FreeBoardRepository.java
@@ -12,10 +12,6 @@ import org.zerock.domain.FreeBoard;
 public interface FreeBoardRepository extends PagingAndSortingRepository<FreeBoard, Long>{
 	public List<FreeBoard> findByBnoGreaterThan(Long bno, Pageable page);
 	
-	@Query("select b.bno, b.title, count(r) from FreeBoard b LEFT OUTER JOIN b.replies r "
-			+ "where b.bno > 0 group by b")
-	public List<Object[]> getPage(Pageable page);
-	
 	//Property or field cannot be found on object of type 'org.springframework.data.domain.PageImpl' 에러 뜸. 이렇게는 못하는듯. 검색조건을 항상 같이 넣어주도록 메소드 짜야겠음
 	//public List<FreeBoard> findAll(Pageable page);
 }
