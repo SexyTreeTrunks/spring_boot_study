b2a5f2295969d24cafdf2646f12dcdf1978dff0d
minjeong <apenr1234@gmail.com>
2018-09-17 00:15:04 +0900

    add index page

---
+++ src/main/java/org/zerock/controller/IndexController.java
@@ -0,0 +1,13 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/index.html")
+	public void index(Model model) {
+		
+	}
+}
